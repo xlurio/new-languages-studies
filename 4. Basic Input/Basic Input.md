45f8ec3e74ef3ed4d959d9496645c32e4df113fc
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-07-11 13:01:18 -0300

    feat: Steps 0, 1, 2, 3 and 4 written

---
+++ 4. Basic Input/Basic Input.md
@@ -0,0 +1,5 @@
+# Basic Input
+
+## Introduction
+
+This is where are the user input basics studies.
---
+++ 4. Basic Input/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name? ");
+string name = Console.ReadLine(); // warning CS8600
+Console.WriteLine($"Hello, {name}!");
