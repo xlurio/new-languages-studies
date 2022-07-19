a11091088339e7f12177ef554885dbb24d87c972
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-07-19 14:03:44 -0300

    learning: added new tests to step 13

--- 13. The Calculator/tests/Integration/CalculatorTests.cs
+++ 13. The Calculator/tests/Integration/CalculatorTests.cs
@@ -21,7 +21,7 @@ class CalculatorTests
 
 
     [Test]
-    public void TestCalculate() {
+    public void TestSum() {
         CalculatorArrangements arrangement = GivenTheSumString();
         decimal result = WhenItsCalculate(arrangement);
         ThenShouldReturnTheSum(result);
@@ -29,6 +29,128 @@ class CalculatorTests
 
     private CalculatorArrangements GivenTheSumString() {
         string argument = "1 + 2";
+
+        return new CalculatorArrangements(
+            new Calculator(),
+            argument
+        );
+    }
+
+    [Test]
+    public void TestSubstraction() {
+        CalculatorArrangements arrangement = GivenTheSubstractionString();
+        decimal result = WhenItsCalculate(arrangement);
+        ThenShouldReturnTheSubstraction(result);
+    }
+
+    private CalculatorArrangements GivenTheSubstractionString() {
+        string argument = "2 - 1";
+
+        return new CalculatorArrangements(
+            new Calculator(),
+            argument
+        );
+    }
+
+    private void ThenShouldReturnTheSubstraction(decimal result) {
+        decimal expected_result = 1;
+        double tolerance = 0.000000001;
+        bool isExpectedResult = 
+            (double)Math.Abs(result - expected_result) <= tolerance;
+
+        Assert.True(isExpectedResult);
+    }
+
+    [Test]
+    public void TestMultiplication() {
+        CalculatorArrangements arrangement = GivenTheMultiplicationString();
+        decimal result = WhenItsCalculate(arrangement);
+        ThenShouldReturnTheMultiplication(result);
+    }
+
+    private CalculatorArrangements GivenTheMultiplicationString() {
+        string argument = "2 * 2";
+
+        return new CalculatorArrangements(
+            new Calculator(),
+            argument
+        );
+    }
+
+    private void ThenShouldReturnTheMultiplication(decimal result) {
+        decimal expected_result = 4;
+        double tolerance = 0.000000001;
+        bool isExpectedResult = 
+            (double)Math.Abs(result - expected_result) <= tolerance;
+
+        Assert.True(isExpectedResult);
+    }
+
+    [Test]
+    public void TestDivision() {
+        CalculatorArrangements arrangement = GivenTheDivisionString();
+        decimal result = WhenItsCalculate(arrangement);
+        ThenShouldReturnTheDivision(result);
+    }
+
+    private CalculatorArrangements GivenTheDivisionString() {
+        string argument = "6 / 2";
+
+        return new CalculatorArrangements(
+            new Calculator(),
+            argument
+        );
+    }
+
+    private void ThenShouldReturnTheDivision(decimal result) {
+        decimal expected_result = 3;
+        double tolerance = 0.000000001;
+        bool isExpectedResult = 
+            (double)Math.Abs(result - expected_result) <= tolerance;
+
+        Assert.True(isExpectedResult);
+    }
+
+    [Test]
+    public void TestInvalidOperatee() {
+        CalculatorArrangements arrangements = GivenTheInvalidOperatee();
+        TestDelegate result = () => WhenItsCalculate(arrangements);
+        ThenShouldRaiseInvalidCalculation(result);
+    }
+
+    private CalculatorArrangements GivenTheInvalidOperatee(){
+        string argument = "a + 3";
+
+        return new CalculatorArrangements(
+            new Calculator(),
+            argument
+        );
+    }
+
+    [Test]
+    public void TestMissingOperator() {
+        CalculatorArrangements arrangements = GivenTheMissingOperator();
+        TestDelegate result = () => WhenItsCalculate(arrangements);
+        ThenShouldRaiseInvalidCalculation(result);
+    }
+
+    private CalculatorArrangements GivenTheMissingOperator() {
+        string argument = "35";
+
+        return new CalculatorArrangements(
+            new Calculator(),
+            argument
+        );
+    }
+
+    public void TestMoreThenOneOperation() {
+        CalculatorArrangements arrangements = GivenMoreThenOneOperator();
+        TestDelegate result = () => WhenItsCalculate(arrangements);
+        ThenShouldRaiseInvalidCalculation(result);
+    }
+
+    private CalculatorArrangements GivenMoreThenOneOperator() {
+        string argument = "3 + 5 + 9";
 
         return new CalculatorArrangements(
             new Calculator(),
@@ -44,24 +166,21 @@ class CalculatorTests
         return calculator.Calculate(argument);
     }
 
-    private void ThenShouldReturnTheSum(decimal sum) {
-        decimal expected_result = 3;
-        double tolerance = 0.000000001;
-        bool isExpectedResult = 
-            (double)Math.Abs(sum - expected_result) <= tolerance;
-
-        Assert.True(isExpectedResult);
+    private void ThenShouldRaiseInvalidCalculation(
+        TestDelegate result
+    ) {
+        Assert.Throws<InvalidCalculationException>(result);
     }
 
     [Test]
-    public void TestInvalidCalculation() {
-        CalculatorArrangements arrangements = GivenTheInvalidCalculation();
-        TestDelegate result = () => WhenItsCalculate(arrangements);
-        ThenShouldRaiseInvalidCalculation(result);
+    public void TestCalculatingNegativeNumbers() {
+        CalculatorArrangements arrangement = GivenTheNegatives();
+        decimal result = WhenItsCalculate(arrangement);
+        ThenShouldReturnTheSum(result);
     }
 
-    private CalculatorArrangements GivenTheInvalidCalculation(){
-        string argument = "a + 3";
+    private CalculatorArrangements GivenTheNegatives() {
+        string argument = "(-)3 + 6";
 
         return new CalculatorArrangements(
             new Calculator(),
@@ -69,9 +188,12 @@ class CalculatorTests
         );
     }
 
-    private void ThenShouldRaiseInvalidCalculation(
-        TestDelegate result
-    ) {
-        Assert.Throws<InvalidCalculationException>(result);
+    private void ThenShouldReturnTheSum(decimal result) {
+        decimal expected_result = 3;
+        double tolerance = 0.000000001;
+        bool isExpectedResult = 
+            (double)Math.Abs(result - expected_result) <= tolerance;
+
+        Assert.True(isExpectedResult);
     }
 }
