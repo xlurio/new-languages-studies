022c3c90ae03fb3baf43ed955ddc58fbf59f46cf
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-12 14:43:22 -0300

    fix: removed build from repo

--- 07. Unit Tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/lucas.correa/Projects/C/new-programming-languages-studies/07. Unit Tests
-# Build directory: C:/Users/lucas.correa/Projects/C/new-programming-languages-studies/07. Unit Tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/lucas.correa/Projects/C/new-programming-languages-studies/07. Unit Tests/build/test_program[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- 07. Unit Tests/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/lucas.correa/Projects/C/new-programming-languages-studies/07. Unit Tests/build/_deps/googletest-src/googlemock
-# Build directory: C:/Users/lucas.correa/Projects/C/new-programming-languages-studies/07. Unit Tests/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
