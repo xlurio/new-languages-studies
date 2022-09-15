9547db76d096d057eed079386b837391c699af48
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-15 12:01:26 -0300

    refactor: merge sort refactored

--- 09. Merge Sort Algorithm/test_merge_sort.cc
+++ 09. Merge Sort Algorithm/test_merge_sort.cc
@@ -10,8 +10,8 @@ TEST(TestMergeSort, test_sorting)
 {
     const int EXPECTED_RESULT[] = {1, 2, 3, 4, 5};
 
-    int RESULT[] = {4, 2, 5, 1, 3};
-    merge_sort(RESULT, 0, 4);
+    int result[] = {4, 2, 5, 1, 3};
+    merge_sort(result, 0, 4);
 
-    EXPECT_THAT(RESULT, ::testing::ElementsAreArray(EXPECTED_RESULT));
+    EXPECT_THAT(result, ::testing::ElementsAreArray(EXPECTED_RESULT));
 }
