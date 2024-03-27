7b49b4c283f726cb36601032fff49019c80e37ff
Lucas Calegarion <lucas.c.calegario@outlook.com>
2024-03-27 14:25:47 -0300

    prac: basic input

---
+++ 04. Basic Input/hello.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h1>Hello <?=$_POST["name"]?>!</h1>
+    </body>
+</html>
---
+++ 04. Basic Input/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <form action="hello.php" method="post">
+            <label for="name">What is your name?</label>
+            <input id="name" name="name">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
